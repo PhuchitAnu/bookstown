06cf6c0d749655ed89a860df63a8a386534d8682
Phuchit Anuchitanukul <blazeofgod@gmail.com>
2025-10-30 08:30:52 +0700

    #

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -3,9 +3,18 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import jwt from "jsonwebtoken";
 
+export const runtime = "nodejs";
+
 export function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
-  const token = req.cookies.get("token")?.value;
+  const cookieHeader = req.headers.get("cookie"); // string ของ cookies ทั้งหมด
+  console.log("[Middleware] cookieHeader:", cookieHeader);
+
+  const token = cookieHeader
+    ?.split(";")
+    .map((c) => c.trim())
+    .find((c) => c.startsWith("token="))
+    ?.split("=")[1];
 
   console.log("[Middleware] pathname:", pathname);
   console.log("[Middleware] all cookies:", req.cookies.getAll());
