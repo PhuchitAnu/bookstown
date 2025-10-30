64ca32364ee219e7377058e3864e8b0ece66b04e
Phuchit Anuchitanukul <blazeofgod@gmail.com>
2025-10-30 08:14:47 +0700

    #

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -3,9 +3,6 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import jwt from "jsonwebtoken";
 
-// ใช้ Node.js runtime
-export const runtime = "nodejs";
-
 export function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
   const token = req.cookies.get("token")?.value;
